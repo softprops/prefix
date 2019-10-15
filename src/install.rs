ccc9bc411c85638c45c6c08bbe869adf89b636d2
softprops <d.tangren@gmail.com>
2019-10-15 08:16:50 -0400

    list of valid hooks

--- src/install.rs
+++ src/install.rs
@@ -1,6 +1,28 @@
 use std::error::Error;
 use structopt::StructOpt;
 
+const HOOKS: &[&str] = &[
+    "applypatch-msg",
+    "pre-applypatch",
+    "post-applypatch",
+    "pre-commit",
+    "prepare-commit-msg",
+    "commit-msg",
+    "post-commit",
+    "pre-rebase",
+    "post-checkout",
+    "post-merge",
+    "pre-push",
+    "pre-receive",
+    "update",
+    "post-receive",
+    "post-update",
+    "push-to-checkout",
+    "pre-auto-gc",
+    "post-rewrite",
+    "sendemail-validate",
+];
+
 #[derive(StructOpt)]
 pub struct Install {}
 
